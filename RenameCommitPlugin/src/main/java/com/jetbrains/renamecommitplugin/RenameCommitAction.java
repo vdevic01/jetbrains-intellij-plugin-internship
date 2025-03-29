0535ee76311e49a110d2104bb702d32d96e4c35f
vdevic01 <vlada.devic.2001@gmail.com>
2025-03-29 18:14:38 +0100

    Registered the action

---
+++ RenameCommitPlugin/src/main/java/com/jetbrains/renamecommitplugin/RenameCommitAction.java
@@ -0,0 +1,13 @@
+package com.jetbrains.renamecommitplugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class RenameCommitAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
